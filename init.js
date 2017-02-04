18124da59a0b06e220f82dcaa717c981419dc85f
Francis Murillo <francisavmurillo@gmail.com>
2017-02-04 21:00:25 +0800

    Adding zenburn theme project

--- init.js
+++ init.js
@@ -38,7 +38,7 @@ define_key(content_buffer_normal_keymap, 'd', 'follow-new-buffer');
 function define_switch_buffer_key(key, buf_num) {
     define_key(default_global_keymap, key, function(I) {
         switch_to_buffer(I.window,
-            I.window.buffers.get_buffer(buf_num));
+                         I.window.buffers.get_buffer(buf_num));
     });
 }
 for (let i = 0; i < 10; ++i) {
@@ -52,3 +52,45 @@ for (let i = 0; i < 10; ++i) {
 theme_load_paths.unshift("~/.conkerorrc/themes/");
 theme_unload("default");
 theme_load("conkeror-theme-zenburn");
+
+
+//set the proxy server for this session only
+proxy_server_default = "127.0.0.1";
+proxy_port_default = 18123;
+
+
+function set_proxy_session (window, server, port) {
+    if (server == "N") {
+        session_pref('network.proxy.type', 0); //direct connection
+        window.minibuffer.message("Direction connection to the internet enabled for this session");
+    } else {
+        if (server == "") server = proxy_server_default;
+        if (port == "") port = proxy_port_default;
+
+        session_pref('network.proxy.ftp',    server);
+        session_pref('network.proxy.gopher', server);
+        session_pref('network.proxy.http',   server);
+        session_pref('network.proxy.socks',  server);
+        session_pref('network.proxy.ssl',    server);
+
+        session_pref('network.proxy.ftp_port',    port);
+        session_pref('network.proxy.gopher_port', port);
+        session_pref('network.proxy.http_port',   port);
+        session_pref('network.proxy.socks_port',  port);
+        session_pref('network.proxy.ssl_port',    port);
+
+        session_pref('network.proxy.share_proxy_settings', true);
+        session_pref('network.proxy.type', 1);
+
+        window.minibuffer.message("All protocols using "+server+":"+port+" for this session");
+    }
+}
+
+interactive("set-proxy-session",
+            "set the proxy server for all protocols for this session only",
+            function (I) {
+                set_proxy_session(
+                    I.window,
+                    (yield I.minibuffer.read($prompt = "server ["+proxy_server_default+"] or N: ")),
+                    (yield I.minibuffer.read($prompt = "port ["+proxy_port_default+"]: ")));
+            });
